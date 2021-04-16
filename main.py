3094f3e35fe4e5638f7c60076e4713dd3cff0564
Micha Reiser <micha@famreiser.ch>
2021-04-16 16:58:59 +0100

    Load Access token from .env

--- main.py
+++ main.py
@@ -4,15 +4,35 @@ import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 
-bot = telegram.Bot(token='TOKEN')
+import os
+from dotenv import load_dotenv
+import logging
 
-updater = Updater(token='TOKEN', use_context=True)
-dispatcher = updater.dispatcher
+
+
+load_dotenv()
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+TOKEN = os.getenv('ACCESS_TOKEN')
 
 def start_test(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-start_handler = CommandHandler('start_test', start_test)
-dispatcher.add_handler(start_handler)
+def main():
+    # bot = telegram.Bot(token=TOKEN)
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('start_test', start_test))
 
-updater.start_polling()
+    updater.start_polling()
+
+if __name__ == '__main__':
+    print(TOKEN)
+    main()
+
+
+
+
+
+

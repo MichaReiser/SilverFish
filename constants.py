5a18fbf5e83f683d39bdc450153ee4e3778bf48e
Micha Reiser <micha@famreiser.ch>
2021-04-16 23:43:33 +0100

    Fix huh gif

--- constants.py
+++ constants.py
@@ -1,2 +1,2 @@
-HUH_FILE_ID = 'https://media.giphy.com/media/xT5LMzlnGiEBhMFfRm/giphy.gif'
+HUH_FILE_ID = 'https://media.giphy.com/media/xT0xeuOy2Fcl9vDGiA/giphy.gif'
 OHOH_FILE_ID = 'https://media.giphy.com/media/xT5LMzlnGiEBhMFfRm/giphy.gif'

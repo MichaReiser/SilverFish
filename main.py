c492a1eea9eaf23eba717a8929176cb191bb86c2
Micha Reiser <micha@famreiser.ch>
2021-04-16 17:26:03 +0100

    Use `Update` instead of `telegram.Update`

--- main.py
+++ main.py
@@ -18,11 +18,10 @@ TOKEN = os.getenv('ACCESS_TOKEN')
 def start_test(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-def handle_text_message(update: telegram.Update, context: CallbackContext):
+def handle_text_message(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Your text: " + update.message.text)
 
 def main():
-    # bot = telegram.Bot(token=TOKEN)
     updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start_test', start_test))

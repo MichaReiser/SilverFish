2dfc1b141462a4fb7c0929a8eb35f8aed1f2a3ba
Micha Reiser <micha@famreiser.ch>
2021-04-16 16:59:18 +0100

    Improve formatting

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ import os
 from dotenv import load_dotenv
 import logging
 
-
-
 load_dotenv()
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')

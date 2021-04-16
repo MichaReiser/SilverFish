f4651bc2e305023f370c130cc5b867c56c79a97f
doemee <doemee@gmail.com>
2021-04-17 00:28:04 +0200

    Update options.py

--- options.py
+++ options.py
@@ -12,14 +12,27 @@ OPTIONS = [
         uri="/sisi", 
         label="Sisi", 
         message="Du interessierst dich für die Kaiserin Elisabeth von Österreich. Ich habe ganz viele Objekte von ihr hier. Soll ich dir etwas über ihre Schuhe oder über ihr Klavier erzählen?",
-        choices=["/",]
+        choices=["/sisi/piano", "/sisi/schuhe"]
     ),
     ChoiceOption(
         uri="/drinkinghabits",
         label="Trinkgewohnheiten",
         message="Kaffee oder Alkohol?",
         choices=["/"]
-    )
+    ),
+      ChoiceOption(
+        uri="/sisi/piano", 
+        label="Klavier", 
+        message="Schau, das Klavier aus dem Musikzimmer von der als Sisi bekannten Kaiserin Elisabeth von Österreich.",
+        choices=["/",]
+    ),
+    ChoiceOption(
+        uri="/sisi/schuhe", 
+        label="Klavier", 
+        message="Schau, das Paar Turnschuhe von der als Sisi bekannten Kaiserin Elisabeth von Österreich, ca. von 1865-1870.",
+        choices=["/",]
+    ),
+    
 ]
 
 BY_URI = {option.uri: option for option in OPTIONS}

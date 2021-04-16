0f181426b79dff9bc0cec962597899cace2e6ce6
Micha Reiser <micha@famreiser.ch>
2021-04-16 17:54:53 +0100

    Add error handler

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackContex
 
 import os
 from dotenv import load_dotenv
-import logging
+import logging          
 
 load_dotenv()
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
 
 TOKEN = os.getenv('ACCESS_TOKEN')
 
@@ -21,11 +22,16 @@ def start_test(update: Update, context: CallbackContext):
 def handle_text_message(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Your text: " + update.message.text)
 
+def handle_error(update: Update, context: CallbackContext):
+    logger.warning('Handling the update "%s" caused an error "%s"', update, context.error)
+    context.bot.send_animation(chat_id=update.effective_chat.id, animation="CgACAgQAAxkBAAMVYHm__nCB5m5X5Ki0dphIG6CQHuoAAjYCAAKXztRSxnPha9rxZUQfBA")
+
 def main():
     updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start_test', start_test))
     dispatcher.add_handler(MessageHandler(filters=Filters.text, callback=handle_text_message))
+    dispatcher.add_error_handler(handle_error)
 
     updater.start_polling()
 

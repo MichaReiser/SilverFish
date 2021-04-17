5131a23261e2489b138c2a44327ef9b07dc4bcb5
Y.L <yl2k@pm.me>
2021-04-17 11:46:04 +0000

    setup pays attention to .env

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,10 +1,19 @@
 #!/bin/dash
 
+skript_pfad=$(realpath $(dirname $0))
+
 frage_ja_nein() {
 	local retval
 	while [ "$retval" != "j" -a "$retval" != "n" ]; do read -p "$1 (j/n) " retval; done
 	echo "$retval"
-} 
+}
+
+frage_wert_oder_default() {
+	local retval
+	read -p "$1 (Standard: $2) " retval;
+	if [ "$retval" = "" ]; then retval="$2"; fi
+	echo "$retval"
+}
 
 user=$(whoami)
 if [ "$user" != "root" ]
@@ -13,8 +22,12 @@ then
 	exit 1;
 fi
 
-printf "\n> Installiere packages via apt install... " && sleep 2 && apt install -y $(cat packages.list) || { echo "Fehler." && exit 1; } && echo "" && echo "Erfolgreich"
+#printf "\n> Installiere packages via apt install... " && sleep 2 && apt install -y $(cat packages.list) || { echo "Fehler." && exit 1; } && echo "" && echo "Erfolgreich"
 
-printf "\n> Installiere python Pakete via pip3... " && sleep 2 && pip3 install $(cat pip.list) || { echo "Fehler." && exit 1; } && echo "" && echo "Erfolgreich"
+#printf "\n> Installiere python Pakete via pip3... " && sleep 2 && pip3 install $(cat pip.list) || { echo "Fehler." && exit 1; } && echo "" && echo "Erfolgreich"
 
-[ "$(frage_ja_nein "> Bot ausführen?")" = "j" ] && { python3 ../main.py; } || echo "Und off..." 
+token="$(frage_wert_oder_default "> Access-Token für Telegram-Bot eingeben" "xxx")"
+
+ls $skript_pfad/../.env 2> /dev/null > /dev/null || { echo "ACCESS_TOKEN=${token}" | tee -a > $skript_pfad/../.env; } && sed -i "s/ACCESS_TOKEN.*/ACCESS_TOKEN=${token}/g" $skript_pfad/../.env
+
+[ "$(frage_ja_nein "> Bot ausführen?")" = "j" ] && { python3 $skript_pfad/../main.py; } || echo "Und off..." 
